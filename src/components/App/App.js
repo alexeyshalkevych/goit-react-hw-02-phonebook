550efe0754ff1a53609db64c4e73e65454a59335
Alexey <valnistuy@gmail.com>
2020-03-22 18:00:44 +0200

    added component ContactList and Contact

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactForm from '../ContactForm/ContactForm';
-import { AppContainer, AppTitle } from './App.styled';
+import ContactList from '../ContactList/ContactList';
+import { AppContainer, AppTitle, AppSubTitle } from './App.styled';
 import './App.css';
 
 export default class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
   };
 
   addContact = contact => {
@@ -29,11 +35,21 @@ export default class App extends Component {
     }));
   };
 
+  deleteContact = id => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
   render() {
+    const { contacts } = this.state;
+
     return (
       <AppContainer>
         <AppTitle>Phonebook</AppTitle>
         <ContactForm onAddContact={this.addContact} />
+        <AppSubTitle>Contacts</AppSubTitle>
+        <ContactList items={contacts} onDeleteContact={this.deleteContact} />
       </AppContainer>
     );
   }
