8c94e66daad8ca7e1a7714dd64a1f85d5474883b
Alexey <valnistuy@gmail.com>
2020-03-22 18:19:33 +0200

    Updates

---
+++ precache-manifest.4a13d2e7fb8191f0a39045ea9dea30ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e6fafa9e5cea896b559bdac28ecb4d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "8d21be78883019929273",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.93b59262.chunk.css"
+  },
+  {
+    "revision": "9adfd70d2e6db16a2b2f",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0d44b9aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0d44b9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d21be78883019929273",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.7b8e4203.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
